2cccfc81940cd63fa93860647dcb2df495f4eaee
刀哥 <liufan321@gmail.com>
2016-05-02 08:34:55 +0800

    init project

---
+++ 导航控制器交互式缩放返回/导航控制器交互式缩放返回/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  导航控制器交互式缩放返回
+//
+//  Created by 刘凡 on 16/5/2.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 导航控制器交互式缩放返回/导航控制器交互式缩放返回/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  导航控制器交互式缩放返回
+//
+//  Created by 刘凡 on 16/5/2.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
